8226d3bb85d05d84b7a51f2e5d3c66236a5ad4d9
ratankumarthakur <144756277+ratankumarthakur@users.noreply.github.com>
2024-01-17 05:23:07 +0530

    main.dart

--- main.dart
+++ main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();                         //this is required to integrate our code with firrebase
   await Firebase.initializeApp(
       options:  FirebaseOptions(
           apiKey: "AIzaSyAvr9wIyrDBEq6Lv1wAl7e6N30FKt9iJME",
@@ -24,140 +24,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: sc(),
       routes: {
-        '/sc2':(context)=>sc2(),
+        '/sc2':(context)=>sc2(),              //defining routes so as to use shortcut of navigation
         '/sc':(context)=>sc(),
 
       },
     ); // MaterialApp
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var wtcontroller = TextEditingController();
-  var ftcontroller = TextEditingController();
-  var incontroller = TextEditingController();
-  var result = "";
-  var bgcolor = Colors.purple.shade200;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("BODY MASS INDEX"),
-          centerTitle: true,
-          backgroundColor: Colors.purple,
-        ), body: Container(
-      color: bgcolor,
-      child: Center(
-        child: Container(
-          width: 300,
-          child: Column(
-            //crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Container(
-              height: 20,
-            ),
-              Image.asset("assets/images/img_1.png", width: 100, height: 100),
-
-              TextField(
-                controller: wtcontroller,
-                decoration: const InputDecoration(
-                    label: Text("enter your weight in kg"),
-                    labelStyle: TextStyle(color: Colors.purple),
-                    prefixIcon: FaIcon(FontAwesomeIcons.weightScale, size: 20,
-                      color: Colors.purple,)
-                ), keyboardType: TextInputType.number,
-              ),
-              Container(
-                height: 10,
-              ),
-
-              TextField(
-                controller: ftcontroller,
-                decoration: const InputDecoration(
-                    label: Text("enter your height in feet"),
-                    labelStyle: TextStyle(color: Colors.purple),
-                    prefixIcon: FaIcon(FontAwesomeIcons.horseHead, size: 20,
-                      color: Colors.purple,)
-                ), keyboardType: TextInputType.number,
-              ),
-              Container(
-                height: 10,
-              ),
-
-              TextField(
-                controller: incontroller,
-                decoration: const InputDecoration(
-                    label: Text("enter your height in inches"),
-                    labelStyle: TextStyle(color: Colors.purple),
-                    prefixIcon: FaIcon(FontAwesomeIcons.horseHead, size: 20,
-                      color: Colors.purple,)
-                ), keyboardType: TextInputType.number,
-              ),
-              Container(
-                height: 50,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(primary: Colors.purple),
-                child: const Text('Calculate',style: TextStyle(color: Colors.white),),
-                onPressed: () {
-                  var wt = wtcontroller.text.toString();
-                  var ft = ftcontroller.text.toString();
-                  var inch = incontroller.text.toString();
-
-                  if (wt != "" && ft != "" && inch != "") {
-                    var iWt = int.parse(wt);
-                    var iFt = int.parse(ft);
-                    var iInch = int.parse(inch);
-
-                    var t1 = (iFt * 12) + iInch;
-                    var t2 = t1 * 2.54;
-                    var t3 = t2 / 100;
-                    var bmi = iWt / (t3 * t3);
-
-
-                    var msg = "";
-
-
-                    if (bmi > 25) {
-                      msg = "You are overweight";
-                      bgcolor = Colors.red.shade400;
-                    } else if (bmi <= 18) {
-                      msg = "You are underweight";
-                      bgcolor = Colors.yellow;
-                    } else {
-                      msg = "You are healthy";
-                      bgcolor = Colors.green.shade500;
-                    }
-                    setState(() {
-                      result = "$msg \nYour BMI is : ${bmi.toStringAsFixed(
-                          2)} ";
-                    });
-                  } else {
-                    setState(() {
-                      result = "please fill all the slots";
-                    });
-                  }
-                },
-              ), Container(
-                height: 20,
-              ),
-              Text(result,
-                style: const TextStyle(fontSize: 24, color: Colors.black),)
-
-            ],
-          ),
-        ),
-      ),
-    ),
-        
-    );
   }
 }
